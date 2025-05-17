3b082375b750a29e21104148d2480cef7513a8ee
Hamid.19471@gmail.com <hamid.19471@gmail.com>
2025-05-17 11:01:37 +0330

    Update: add select component in main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,20 @@
-const App = () => {
-  return <div>Home page</div>;
-};
+import MultiSelect from "./components/multi-select";
+import { OPTIONS_DUMMY } from "./constants/options-dummy";
+
+function App() {
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <MultiSelect options={OPTIONS_DUMMY} />
+    </div>
+  );
+}
+
 export default App;
