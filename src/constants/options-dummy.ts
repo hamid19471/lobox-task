ac5c538d13d364ac670c743eca423d36a9314270
Hamid.19471@gmail.com <hamid.19471@gmail.com>
2025-05-17 11:01:24 +0330

    Create: a dummy data for use in select component as option

---
+++ src/constants/options-dummy.ts
@@ -0,0 +1,8 @@
+export const OPTIONS_DUMMY = [
+  { label: "Education", icon: "🎓" },
+  { label: "Yeeeah, science!", icon: "🧪" },
+  { label: "Art", icon: "🎨" },
+  { label: "Sport", icon: "⚽" },
+  { label: "Games", icon: "🎮" },
+  { label: "Health", icon: "🏥" },
+];
